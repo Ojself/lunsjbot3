4529ff7f6bf84b338d30393707b61a98d240ee22
Tormod Flesjø <tormod.flesjo@gmail.com>
2025-07-28 09:13:29 +0200

    quick commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const cheerio = require('cheerio');
-const cron = require('node-cron');
+// const cron = require('node-cron');
 const { IncomingWebhook } = require('@slack/webhook');
 const OpenAI = require('openai');
 const puppeteer = require('puppeteer');
@@ -22,7 +22,7 @@ const s3 = new S3Client({
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
   },
-  forcePathStyle: true, // <--- Add this line
+  forcePathStyle: true,
 });
 
 async function uploadToR2(imageUrl, fileName) {
